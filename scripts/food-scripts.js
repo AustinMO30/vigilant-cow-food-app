690794351176546d455134251eb528c6f2b4e4a3
Austin Moses <austinmoses18@gmail.com>
2024-07-11 13:50:58 -0500

    Add files via upload
    Updated APP!

--- scripts/food-scripts.js
+++ scripts/food-scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
   console.log("ready!");
+
+  // Issue1: Click event for "Load Foods" button
+  $('#btnLoadFoods').click(function() {
+    $('#foodList').empty(); // Clear the existing food list
+
+    // Issue2: target food list container
+    foodList.forEach(function(food) {
+      $('#foodList').append(`<li class="list-group-item"><i class="fas fa-utensils"></i> ${food}</li>`);
+    });
+
+    $('#foodList').slideDown(2000);
+  });
+
+  // Issue2: target food list container
+  $('#btnClearFoods').on('click', function() {
+    $('#foodList').slideUp(1000, function() {
+      $(this).empty();
+    });
+  });
+  
+  
+  let foodList = [
+    "Prime Rib",
+    "Flank Steak",
+    "Loaded Mac and Cheese",
+    "Barbeque Tacos",
+    "Shrimp Alfredo",
+    "Chicken Fingers"
+  ];
+
 });
